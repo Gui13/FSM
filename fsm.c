a9f15131255676ab8cd9648eee3e3ff84f41bfa8
Guillaume B <guitreize@gmail.com>
2010-09-10 07:42:44 +0200

    Revert "License"
    This reverts commit f9cd25bb971fcc9ddbd879d9cc554658a809af82.

--- fsm.c
+++ fsm.c
@@ -3,22 +3,6 @@
  *
  *  Created on: 3 août 2010
  *      Author: gui13
- *      Contact: guitreize@gmail.com
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
  */
 
 #include <stdlib.h>
